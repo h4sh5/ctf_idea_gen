a3d174c29f10420640cb8b921ef965f4030c6a0c
Haoxi Tan <haoxi.tan@gmail.com>
2023-06-29 23:29:15 +1000

    ctf idea generator

---
+++ ctfidea.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import random
+
+category_file = 'categories.txt'
+words_file = 'words.txt'
+
+categories = open(category_file,'r').read().split('\n')
+categories.remove('')
+words = open(words_file,'r').read().split('\n')
+words.remove('')
+
+while True:
+	input('next? ')
+	# choose 1 category and 2 words
+	# cat = random.choice(categories)
+	word1 = random.choice(words)
+	word2 = random.choice(words)
+	word3 = random.choice(words)
+	print(word1, word2, word3)
